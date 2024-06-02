4ded486d5ba799fd4bf495470fe85ab0a7b950dd
nezz <72879112+nezzkenya@users.noreply.github.com>
2024-06-02 18:48:50 +0300

    Update ComingGames.js

--- ComingGames.js
+++ ComingGames.js
@@ -11,7 +11,7 @@ export default async function ComingGames() {
     const browser = await puppeteer.launch({
       headless: true,
       args: ["--disable-setuid-sandbox", "--no-sandbox"],
-      executablePath: process.env.CHROME_BIN || null,
+      executablePath: process.env.GOOGLE_CHROME_BIN  || null,
     });
     browserInstances.push(browser);
 
