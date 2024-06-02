182c5bfa24cfa1d731e1ad692a6e64deb60c01d4
nezz mogambi <nezzkenya02@gmail.com>
2024-06-02 15:58:42 +0300

    s

--- ComingGames.js
+++ ComingGames.js
@@ -8,7 +8,19 @@ export default async function ComingGames() {
   const browserInstances = [];
 
   try {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
+    });
     browserInstances.push(browser);
 
     const page = await browser.newPage();
