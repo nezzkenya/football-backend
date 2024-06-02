abca290ba8bdfc43d1447cd78481562b73a16f7c
nezz mogambi <nezzkenya02@gmail.com>
2024-06-02 13:54:59 +0300

    removed f

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Call GetGames initially
-GetGames();
-main();
-ComingGames();
+
 // Set interval to call GetGames every 5 minutes
 const intervalId = setInterval(GetGames, 5 * 60 * 1000);
 const intervalId2 = setInterval(main, 5 * 60 * 1000);
