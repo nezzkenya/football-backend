0592c8e829c4e03316fb591c4bcf8c5364f2adfa
nezz <72879112+nezzkenya@users.noreply.github.com>
2024-06-04 22:46:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,8 @@ const app = express();
 
 // Call GetGames initially
 GetGames();
-main();
 // Set interval to call GetGames every 5 minutes
-const intervalId = setInterval(GetGames, 5 * 60 * 1000);
-const intervalId2 = setInterval(main, 5 * 60 * 1000);
+const intervalId = setInterval(GetGames, 5 * 60 * 1000)
 
 app.listen(PORT, () => {
   console.log(`App is running and listening on port ${PORT}`);
