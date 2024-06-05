4aa171818ae7f80c0ef642a350d77589b755d27a
nezz <72879112+nezzkenya@users.noreply.github.com>
2024-06-05 20:25:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 import cors from "cors";
 import express from "express";
 import "dotenv/config";
-import GetGames from "./Games.js";
 import Routes from "./routes.js";
 import main from "./DeleteGames.js";
-import ComingGames from "./ComingGames.js";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-
+const R1 = async () => {
+  const res = await fetch("https://football-backend-2.onrender.com/")
+  const data = await res.json()
+  console.log(data)
+}
+const R2 = async () => {
+  const res = await fetch("https://football-backend-3.onrender.com/")
+  const data = await res.json()
+  console.log(data)
+}
 // Call GetGames initially
-GetGames();
 main();
-ComingGames();
 // Set interval to call GetGames every 5 minutes
-const intervalId = setInterval(GetGames, 5 * 60 * 1000);
-const intervalId2 = setInterval(main, 5 * 60 * 1000);
-const intervalId3 = setInterval(ComingGames, 60 * 60 * 1000);
+const intervalId2 = setInterval(main, 20 * 60 * 1000);
+const intervalId2 = setInterval(R1, 3 * 60 * 1000);
+const intervalId2 = setInterval(R2, 3 * 60 * 1000);
 app.use(cors());
 app.use(express.json());
 app.use("/api", Routes);
