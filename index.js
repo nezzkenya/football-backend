d34393c1c2f5e5694dd68904ab5af98bd35b38f1
nezz <72879112+nezzkenya@users.noreply.github.com>
2024-06-06 17:04:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 // Call GetGames initially
 ComingGames();
 // Set interval to call GetGames every 5 minutes
-const intervalId3 = setInterval(ComingGames, 60 * 60 * 1000);
+const intervalId3 = setInterval(ComingGames, 20 * 60 * 1000);
 app.get("/", async (req,res)=>{
   res.send("hello there")
 }
