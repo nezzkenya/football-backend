2716b7cf3bdea3bf1072a1ac89b3f731487e01f9
nezz <72879112+nezzkenya@users.noreply.github.com>
2024-06-09 08:49:43 +0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -2,11 +2,40 @@ import express from "express";
 import db from "./connection.js";
 const router = express.Router();
 
+// Function to filter games
+function filterGames(games) {
+  const now = new Date();
+  
+  return games.filter(game => {
+    if (game.time === "24/7") {
+      return true;
+    }
+    
+    const [time, period] = game.time.split(' ');
+    const [hours, minutes] = time.split(':').map(Number);
+    const gameTime = new Date(now);
+
+    if (period === 'PM' && hours !== 12) {
+      gameTime.setHours(hours + 12);
+    } else if (period === 'AM' && hours === 12) {
+      gameTime.setHours(0);
+    } else {
+      gameTime.setHours(hours);
+    }
+
+    gameTime.setMinutes(minutes);
+
+    return gameTime <= now;
+  });
+}
+
+
 router.get("/allgames", async (req, res) => {
   try {
     const collection = await db.collection("all-games");
     const results = await collection.find({}).toArray();
-    res.json(results).status(200);
+    const cg = await  filterGames(results)
+    res.json(cg).status(200);
   } catch (error) {
     res.json("an error occurred").status(500);
     console.log(error);
