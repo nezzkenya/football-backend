d598dbf7c3686c68eeeeb6426ecc2474411c298a
nezz4 <mogambisomo+1@gmail.com>
2024-07-27 16:02:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ async function fetchGames() {
 }
 async function fetchYouTube() {
   try {
-    const response = await fetch("https://youtube-project-cil7.onrender.com");
+    const response = await fetch("https://youtube-project-3-lljx.onrender.com");
     const data = await response;
     console.log(data, "\nYouTube kept alive");
   } catch (error) {
@@ -30,7 +30,6 @@ async function fetchYouTube() {
 
 fetchGames();
 fetchYouTube();
-fetchTg();
 
 setInterval(fetchGames, REFRESH_INTERVAL);
 setInterval(fetchYouTube, REFRESH_INTERVAL);
