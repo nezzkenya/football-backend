e2aff23f5ec9cd4f79d0b511ad9818f8bf512eeb
nezz4 <mogambisomo+1@gmail.com>
2024-08-10 12:32:52 +0000

    timed

--- Gamehandlers.js
+++ Gamehandlers.js
@@ -1,31 +1,42 @@
 import db from "./connection.js";
 
-export async function getLivegames(req,res) {
-    try {  
+export async function getLivegames(req, res) {
+    try {
         const collection2 = await db.collection("games");
-    
+
         const pipeline = [
-          {
-            $group: {
-              _id: "$Name",
-              document: { $first: "$$ROOT" },
+            {
+                $group: {
+                    _id: "$Name",
+                    document: { $first: "$$ROOT" },
+                },
             },
-          },
-          {
-            $replaceRoot: { newRoot: "$document" },
-          },
-          {
-            $project: {
-              Name: 1,
-              link: 1,
-              _id: 0
+            {
+                $replaceRoot: { newRoot: "$document" },
             },
-          },
+            {
+                $project: {
+                    Name: 1,
+                    link: 1,
+                    time: 1,
+                    _id: 0
+                },
+            },
+            {
+                $facet: {
+                    "allDayGames": [
+                        { $match: { time: "24/7" } }
+                    ],
+                    "timedGames": [
+                        { $match: { time: { $regex: /^[0-9]{1,2}:[0-9]{2} [AP]M$/ } } }
+                    ]
+                }
+            }
         ];
-    
+
         const results2 = await collection2.aggregate(pipeline).toArray();
-        res.status(201).json({games : results2})
+        res.status(201).json(results2);
     } catch (error) {
-        
+        res.status(500).json({ error: error.message });
     }
 }
